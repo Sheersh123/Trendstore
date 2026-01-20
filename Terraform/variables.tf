2586a976b366aa3a88d1ddd778685acc4385ab77
Sheersh <sheershsinha30@gmail.com>
2026-01-20 16:24:00 +0530

    Modified-variables-and-tfvars-files:terraform

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -23,7 +23,7 @@ variable "public_subnet_cidrs" {
 variable "private_subnet_cidrs" {
   description = "Two private subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  default     = ["", ""]
 }
 
 variable "main_cidr" {
